0b10c2ed1ff25e2eac941d60da95177109f79ba2
Onur Sezen <onur@palnea.com>
2024-09-17 23:36:15 +0300

    Remove drawer menu

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,13 +1,10 @@
 <script setup>
-import Drawer from "@/layouts/Drawer.vue";
 import TopBar from "@/components/app/TopBar.vue";
 </script>
 
 <template>
   <TopBar>
-    <Drawer>
       <router-view></router-view>
-    </Drawer>
   </TopBar>
 </template>
 
