2004d9f8e5435b6197ac58530557a63ba36ba7b4
Onur Sezen <osezen@cyangate.com>
2025-05-29 19:29:01 +0300

    Add confirm dialog

--- src/components/common/ConfirmDialog.vue
+++ src/components/common/ConfirmDialog.vue
@@ -1,62 +1,89 @@
 <template>
-  <v-dialog v-model="dialog" persistent max-width="290">
-    <v-card>
-      <v-card-title class="headline text-center">{{ title }}</v-card-title>
-      <v-card-text class="text-subtitle-1 text-center">{{ message }}</v-card-text>
-      <v-card-actions class="justify-center">
-        <v-btn color="red darken-1" text @click="cancel">Cancel</v-btn>
-        <v-btn color="green darken-1" text @click="confirm">OK</v-btn>
+  <v-dialog v-model="dialog" max-width="450px">
+    <v-card class="flex-row-center pa-5">
+      <v-card-title class="mb-5">
+        <span class="headline">{{ heading }}</span>
+      </v-card-title>
+      <v-card-text style="width: 80%">
+        <p class="text-body-1 mb-3">{{ message }}</p>
+        <p v-if="subMessage" class="text-body-2 text-medium-emphasis">{{ subMessage }}</p>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+          class="no-uppercase mr-2"
+          color="#003058"
+          variant="outlined"
+          @click="cancel"
+        >
+          {{ cancelText }}
+        </v-btn>
+        <v-btn
+          :color="confirmColor"
+          class="no-uppercase"
+          variant="flat"
+          @click="confirm"
+        >
+          {{ confirmText }}
+        </v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
 
 <script setup>
-import {ref, watchEffect, watch} from 'vue';
+import { ref, watch } from 'vue';
+import { COLORS } from "@/styles/colors";
 
 const props = defineProps({
-  title: String,
-  message: String,
-  onConfirm: Function,
-  onCancel: Function,
+  modelValue: Boolean,
+  heading: {
+    type: String,
+    default: 'Confirm Action'
+  },
+  message: {
+    type: String,
+    default: 'Are you sure you want to proceed?'
+  },
+  subMessage: {
+    type: String,
+    default: null
+  },
+  confirmText: {
+    type: String,
+    default: 'Delete'
+  },
+  cancelText: {
+    type: String,
+    default: 'Cancel'
+  },
+  confirmColor: {
+    type: String,
+    default: 'error'
+  }
 });
 
-const dialog = ref(false);
+const emit = defineEmits(['update:modelValue', 'confirm', 'cancel']);
+
+const dialog = ref(props.modelValue);
+
+watch(() => props.modelValue, (newVal) => {
+  dialog.value = newVal;
+});
+
+watch(dialog, (newVal) => {
+  if (!newVal) {
+    emit('update:modelValue', false);
+  }
+});
 
 const confirm = () => {
-  dialog.value = false;
-  props.onConfirm();
-  emit('update:modelValue', dialog.value);
+  emit('confirm');
+  emit('update:modelValue', false);
 };
 
 const cancel = () => {
-  dialog.value = false;
-  if (props.onCancel) {
-    props.onCancel();
-  }
-  emit('update:modelValue', dialog.value);
+  emit('cancel');
+  emit('update:modelValue', false);
 };
-
-watchEffect(() => {
-  dialog.value = props.modelValue;
-});
-
-const emit = defineEmits(['update:modelValue']);
-
-watch(
-  () => dialog.value,
-  (newValue) => {
-    emit('update:modelValue', newValue);
-  }
-);
 </script>
-
-<style scoped>
-.headline {
-  color: #222222;
-}
-
-.text-subtitle-1 {
-  color: #757575;
-}
-</style>
